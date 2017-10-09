ff6901acfa83888d591ccfe57baab69374000bc0
Dmitry <dmitry-orlov@hotmail.com>
2017-10-09 09:55:05 +0200

    a visual update

--- resources/views/func/f_qual.blade.php
+++ resources/views/func/f_qual.blade.php
@@ -10,26 +10,30 @@
             @endforeach
         @endif
 </div>
-{!! Form::open(['url' => '/record']) !!}
-<!-- The techers list -->
-<ul class="mdl-list">
-    @foreach ($teachers as $teacher)
-  <li class="mdl-list__item mdl-list__item--two-line">
-    <span class="mdl-list__item-primary-content">
-      <i class="material-icons mdl-list__item-avatar">person</i>
-      <span>{{$teacher->name}}</span>
-      <span class="mdl-list__item-sub-title"><a href="{{$teacher->link}}" target="_blank">more...</a></span>
-    </span>
-    <span class="mdl-list__item-secondary-action">
-      <label class="demo-list-radio mdl-radio mdl-js-radio mdl-js-ripple-effect" for="list-a-option-{{$teacher->id}}">
-        <input type="radio" id="list-a-option-{{$teacher->id}}" class="mdl-radio__button" name="teachers" value="{{$teacher->id}}"/>
-      </label>
-    </span>
-  </li>
-  @endforeach
-</ul>
-{!! Form::submit('Continue',["class"=>"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"]); !!}
-{!! Form::close() !!}
+<div class="mdl-grid">
+  <div class="mdl-cell mdl-cell--8-col mdl-cell--2-offset">
+    {!! Form::open(['url' => '/record']) !!}
+    <!-- The techers list -->
+    <ul class="mdl-list">
+        @foreach ($teachers as $teacher)
+      <li class="mdl-list__item mdl-list__item--two-line">
+        <span class="mdl-list__item-primary-content">
+          <i class="material-icons mdl-list__item-avatar">person</i>
+          <span>{{$teacher->name}}</span>
+          <span class="mdl-list__item-sub-title"><a href="{{$teacher->link}}" target="_blank">more...</a></span>
+        </span>
+        <span class="mdl-list__item-secondary-action">
+          <label class="demo-list-radio mdl-radio mdl-js-radio mdl-js-ripple-effect" for="list-a-option-{{$teacher->id}}">
+            <input type="radio" id="list-a-option-{{$teacher->id}}" class="mdl-radio__button" name="teachers" value="{{$teacher->id}}"/>
+          </label>
+        </span>
+      </li>
+      @endforeach
+    </ul>
+    {!! Form::submit('Continue',["class"=>"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"]); !!}
+    {!! Form::close() !!}
+  </div>
+</div>
 
 @endsection
 
--- resources/views/mistake.blade.php
+++ resources/views/mistake.blade.php
@@ -8,6 +8,10 @@
 @endsection
 @section('subtitle')
     We will make sure everything is correct now,<br/> start again <a href="/">here</a> :)
+    <br/>
+    <br/>
+    To report the problem contact 
+        <a href="https://www.facebook.com/FontysProxy" target="_blank">Proxy</a>
     <!--2017-->
     <!--The winner is to be announced at the P-Certificate ceremony-->
 @endsection
